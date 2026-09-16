f1f734fff4e40abac89e51ce7aaf0091872fb614
bhojarajangadi <bhojarajangadi9@gmail.com>
2026-09-16 09:47:00 +0530

    Fix spelling errors in even/odd messages
    Corrected spelling of 'divisible' in output messages.

--- evenorodd.py
+++ evenorodd.py
@@ -2,7 +2,7 @@ num = int(input("Enter a number: "))
 
 if num % 2 == 0:
     print("The number is even.")
-    print("This number is divisible by 2.")
+    print("This number is divisiable by 2.")
 else:
     print("The number is odd.")
-    print("This number is not divisible by 2.")
+    print("This number is not divisiable by 2.")
